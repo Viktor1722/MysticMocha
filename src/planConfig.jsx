572055f1635d123d1cf2a7f7977c3585a4aff1bf
viktor.velizarov <viktor.velizarov@bycape.io>
2024-01-29 16:26:45 +0100

    added the main page, cardFlip,planConfig as well as the styling for them

---
+++ src/planConfig.jsx
@@ -0,0 +1,16 @@
+const planFeatures = {
+  basic: {
+    socialMediaLinks: false,
+    otherFeature: true,
+  },
+  medium: {
+    socialMediaLinks: true,
+    otherFeature: true,
+  },
+  premium: {
+    socialMediaLinks: true,
+    otherFeature: true,
+  },
+};
+
+export default planFeatures;
