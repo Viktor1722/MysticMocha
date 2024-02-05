b6e17ce987ce1f2643f570bfffa470581c1c67d5
viktor.velizarov <viktor.velizarov@bycape.io>
2024-02-05 17:33:01 +0100

    fixed the error in the Firebase config file

--- src/fireBaseConfig.js
+++ src/fireBaseConfig.js
@@ -5,14 +5,6 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const {
-  getFirestore,
-  collection,
-  writeBatch,
-  doc,
-} = require("firebase/firestore");
-const wishes = require("./wishes.json");
-
 const firebaseConfig = {
   apiKey: "AIzaSyDkuXN_cHktCO5iQ-dgddCiDPKmFGY0ssw",
   authDomain: "lucky-coffee-eb037.firebaseapp.com",
@@ -25,20 +17,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
-const uploadWishes = async (wishes) => {
-  const batch = writeBatch(db);
-  const wishesCol = collection(db, "wishes");
-
-  wishes.forEach((wish) => {
-    const docRef = doc(wishesCol); // Create a new document reference
-    batch.set(docRef, { text: wish }); // Add your wish
-  });
-
-  await batch.commit();
-  console.log("All wishes have been uploaded.");
-};
-
-uploadWishes(wishes);
 
 export { db };
