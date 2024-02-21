0b5dd0b33fb493b26f478c6225afbb7c0ca91879
viktor.velizarov <viktor.velizarov@bycape.io>
2024-02-21 13:10:47 +0100

    test

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import FlipCard from "./cardFlip";
-import planFeatures from "../planConfig";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { db } from "../fireBaseConfig";
 import html2canvas from "html2canvas";
@@ -60,17 +59,6 @@ function MainPage() {
   const goToSharePage = () => {
     navigate("/share", { state: { wish: fortune } }); // Pass fortune as state
   };
-
-  const featureAccess = planFeatures[userPlan];
-  const twitterShareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-    fortune
-  )}`;
-  const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-    document.location.href
-  )}&quote=${encodeURIComponent(fortune)}`;
-
-  const googleReviewLink =
-    "https://www.google.com/maps/place/[https://www.google.com/maps/place/Cherry+by+Mary/@43.2075205,27.9218619,53m/data=!3m1!1e3!4m6!3m5!1s0x40a4551d2e10fb85:0x518af4d8d4b7711!8m2!3d43.2074796!4d27.9217361!16s%2Fg%2F11qz39lctt?entry=ttu";
 
   return (
     <div className="container">
